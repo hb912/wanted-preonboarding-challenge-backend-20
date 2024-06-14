d9213485e37a04f5e7885cefa12c019b2d9935f4
chaehong kim <43055448+hb912@users.noreply.github.com>
2024-06-14 09:15:41 +0900

    feat: messageSource 추가

---
+++ src/main/java/com/example/wanted_market/global/config/MessageSourceConfig.java
@@ -0,0 +1,24 @@
+package com.example.wanted_market.global.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfig {
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public MessageSourceAccessor getMessageSourceAccessor() {
+        ReloadableResourceBundleMessageSource m = messageSource();
+        return new MessageSourceAccessor(m);
+    }
+}
